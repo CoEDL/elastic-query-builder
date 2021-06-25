3b34db5aa23aa39f4defb2966deced24249c0380
Marco La Rosa <m@lr.id.au>
2021-06-26 09:24:05 +1000

    initial commit

---
+++ src/elastic-builder.spec.js
@@ -0,0 +1,6 @@
+import * as eb from "./elastic-builder";
+console.log(eb);
+
+describe("Test search builder capabailities", () => {
+    test("it should be able to run a term query on a keyword field", async () => {});
+});
