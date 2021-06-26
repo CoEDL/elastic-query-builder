d633c9e1bb7391add04447cc573143924bf290ee
Marco La Rosa <m@lr.id.au>
2021-06-26 16:16:04 +1000

    implement bool query class and tests

--- src/bool-query.ts
+++ src/bool-query.ts
@@ -1,20 +1,53 @@
+import { BoolQueryResponseInterface } from "./interfaces";
+
 export class BoolQuery {
-    private _queries: any[];
-    
+    private _must: any[];
+    private _should: any[];
+    private _filter: any[];
+    private _mustNot: any[];
+
     constructor() {
-        this._queries = []
+        this._must = [];
+        this._should = [];
+        this._filter = [];
+        this._mustNot = [];
     }
-    must(query: any[]) {
-        this._queries.push(query);
+    must(query: {}): this {
+        this._must.push(query);
         return this;
     }
-    filter() {
+    filter(query: {}): this {
+        this._filter.push(query);
         return this;
     }
-    should() {
+    should(query: {}): this {
+        this._should.push(query);
         return this;
     }
-    mustNot() {
+    mustNot(query: {}): this {
+        this._mustNot.push(query);
         return this;
+    }
+    toJSON(): BoolQueryResponseInterface {
+        let json: BoolQueryResponseInterface = {
+            bool: {},
+        };
+
+        const props = ["_must", "_should", "_filter", "_mustNot"] as const;
+        for (let name of props) {
+            if (this[name].length) {
+                let result = this[name].map((acc: {}, query: {}) => ({
+                    ...acc,
+                    ...query,
+                }));
+                const key = `${name.replace("_", "")}`;
+                json.bool = { ...json.bool, [key]: result };
+            }
+        }
+        return json;
+    }
+
+    toJson() {
+        return this.toJSON();
     }
 }
---
+++ src/bool.spec.js
@@ -0,0 +1,61 @@
+const { BoolQuery, matchPhraseQuery, matchQuery } = require("../dist/cjs");
+
+describe("Test query constructor capabilities", () => {
+    test("it should be able instantiate a new boolean query with default options", () => {
+        let bool = new BoolQuery();
+        let json = bool.toJSON();
+        expect(json).toEqual({ bool: {} });
+    });
+    test("it should be able to set a must clause on the bool query", () => {
+        let bool = new BoolQuery();
+        let phraseQuery = matchPhraseQuery({ field: "type.keyword", value: "person" });
+        bool = bool.must(phraseQuery);
+        let json = bool.toJSON();
+        expect(json).toEqual({
+            bool: {
+                must: [
+                    {
+                        match_phrase: {
+                            "type.keyword": "person",
+                        },
+                    },
+                ],
+            },
+        });
+    });
+    test("it should be able to set multiple must clauses on the bool query", () => {
+        let bool = new BoolQuery();
+        bool = bool.must(matchPhraseQuery({ field: "type.keyword", value: "person" }));
+        bool = bool.must(matchQuery({ field: "type.keyword", value: "person" }));
+        let json = bool.toJSON({
+            bool: {
+                must: [
+                    {
+                        match_phrase: {
+                            "type.keyword": "person",
+                        },
+                    },
+                ],
+            },
+        });
+        expect(json).toEqual({
+            bool: {
+                must: [
+                    {
+                        match_phrase: {
+                            "type.keyword": "person",
+                        },
+                    },
+                    {
+                        match: {
+                            "type.keyword": {
+                                query: "person",
+                                operator: "AND",
+                            },
+                        },
+                    },
+                ],
+            },
+        });
+    });
+});
