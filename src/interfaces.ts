2dbbb0f6342bffde92fc2e49cabe901c64ce6286
Marco La Rosa <m@lr.id.au>
2021-06-28 13:14:19 +1000

    set query param optional

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,14 +1,15 @@
 export interface QueryBodyInterface {
     size: number;
     from: number;
-    query: {};
+    query?: {};
     aggs?: {};
 }
 
 export interface QueryResponseInterface {
     size: number;
     from: number;
-    query: {};
+    query?: {};
+    aggs?: {};
 }
 
 export interface BoolQueryResponseInterface {
