16ffecfa6b3b27f37413eae3b8c6db0ca98a57a0
Marco La Rosa <m@lr.id.au>
2021-06-28 10:12:25 +1000

    cleanup

---
+++ src/helpers.ts
@@ -0,0 +1,31 @@
+import { Headers } from "headers-utils";
+const fetch = require("node-fetch");
+
+export async function execute({
+    service,
+    index,
+    query,
+}: {
+    service: string;
+    index: string;
+    query: {};
+}) {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    index = index ? `${service}/${index}/_search` : `${service}/${index}`;
+    let response = await fetch(index, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(query),
+    });
+    if (response.status !== 200) {
+        console.log((await response.json()).error);
+        return {};
+    }
+    response = await response.json();
+    const total = response.hits.total.value;
+    const aggregations = response.aggregations;
+    const documents = response.hits.hits;
+    return { total, documents, aggregations };
+}
