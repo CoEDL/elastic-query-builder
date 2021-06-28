0add121d52532f1faf21047f04a904b59dccf243
Marco La Rosa <m@lr.id.au>
2021-06-28 10:44:18 +1000

    fixup

--- src/queries/full-text-queries/match-query.spec.js
+++ src/queries/full-text-queries/match-query.spec.js
@@ -1,6 +1,6 @@
 const { matchQuery } = require("../../../dist/cjs/queries");
 
-test("it should be able to construct term queries", async () => {
+test("it should be able to construct match queries", async () => {
     let q = matchQuery({ field: "type", value: "person" });
     expect(q).toEqual({ match: { type: { query: "person", operator: "AND" } } });
 });
