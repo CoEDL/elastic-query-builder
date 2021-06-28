1eb0233cb57c8de42abe134a3c347dd68570060a
Marco La Rosa <m@lr.id.au>
2021-06-28 13:15:51 +1000

    disable nested aggregation code for now

--- src/aggregations/nested-aggregation.spec.js
+++ src/aggregations/nested-aggregation.spec.js
@@ -1,28 +1,28 @@
-const { nestedAggregation } = require("../../dist/cjs/aggregations");
+// const { nestedAggregation } = require("../../dist/cjs/aggregations");
 
 test("it should be able to construct a nested aggregation", async () => {
-    let a = nestedAggregation({ path: "type", field: "keyword" });
-    expect(a).toEqual({
-        size: 0,
-        aggs: {
-            type: {
-                nested: {
-                    path: "type",
-                },
-                aggs: {
-                    values: {
-                        terms: {
-                            field: "type.keyword",
-                            size: 10,
-                        },
-                    },
-                    count: {
-                        cardinality: {
-                            field: "type.keyword",
-                        },
-                    },
-                },
-            },
-        },
-    });
+    //     let a = nestedAggregation({ path: "type", field: "keyword" });
+    //     expect(a).toEqual({
+    //         size: 0,
+    //         aggs: {
+    //             type: {
+    //                 nested: {
+    //                     path: "type",
+    //                 },
+    //                 aggs: {
+    //                     values: {
+    //                         terms: {
+    //                             field: "type.keyword",
+    //                             size: 10,
+    //                         },
+    //                     },
+    //                     count: {
+    //                         cardinality: {
+    //                             field: "type.keyword",
+    //                         },
+    //                     },
+    //                 },
+    //             },
+    //         },
+    //     });
 });
